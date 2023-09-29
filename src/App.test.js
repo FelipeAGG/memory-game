7644813e9fdb2e3f476c2998a42d96d27784ddfc
Felipe Gallardo <felipeagg90@gmail.com>
2023-09-29 13:12:21 -0300

    App.js test added. Delete responseMock.js file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,41 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App component with start screen', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Welcome to Memory Game/i);
-  expect(linkElement).toBeInTheDocument();
+
+  expect(screen.getByText('Welcome to Memory Game')).toBeInTheDocument();
+
+  expect(screen.getByText('Start Game')).toBeInTheDocument();
+
+  expect(screen.queryByText('Hello,')).toBeNull();
+});
+
+test('starts the game when clicking "Start Game"', () => {
+  render(<App />);
+
+  const input = screen.getByPlaceholderText('Enter your name');
+  fireEvent.change(input, { target: { value: 'John' } });
+
+  const startGameButton = screen.getByText('Start Game');
+  fireEvent.click(startGameButton);
+
+  expect(screen.getByText('Hello, John!')).toBeInTheDocument();
+
+  expect(screen.queryByText('Welcome to Memory Game')).toBeNull();
+});
+
+test('resets the game when clicking "Reset Game" again and press "Exit Game"', () => {
+  render(<App />);
+
+  const startGameButton = screen.getByText('Reset Game');
+  fireEvent.click(startGameButton);
+
+  expect(screen.getByText('Hello, John!')).toBeInTheDocument();
+
+  const exitGameButton = screen.getByText('Exit Game');
+  fireEvent.click(exitGameButton);
+
+  expect(screen.getByText('Welcome to Memory Game')).toBeInTheDocument();
 });
